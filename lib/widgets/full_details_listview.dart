c975df4b92d038d06e4ebe7cc2f35f4a2151766a
Mohamed Shaduli <mshadulykk76@gmail.com>
2023-04-03 10:30:21 +0530

    bug fixes

--- lib/widgets/full_details_listview.dart
+++ lib/widgets/full_details_listview.dart
@@ -21,84 +21,89 @@ class FullDetailsListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                    height: 200,
-                    child: photo == null
-                        ? const CircleAvatar(
-                            backgroundImage:
-                                AssetImage('Assets/images/avatar.jpg'),
-                            radius: 80,
-                          )
-                        :
-                        // CircleAvatar(
-                        //     backgroundImage: FileImage(
-                        //       File(photo!),
-                        //     ),
-                        //     radius: 90,
-                        //   ),
-                        SizedBox(
-                            child: Image(
-                              image: FileImage(
-                                File(photo!),
-                              ),
-                            ),
-                          )),
-              ],
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 50,
-                    child: Text(
-                      'NAME :  $name',
-                      style: const TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                    child: Text(
-                      'CLASS :  $class_',
-                      style: const TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                    child: Text(
-                      'AGE : $age',
-                      style: const TextStyle(
-                          fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                    child: Text(
-                      'PHONE NO : $rollno',
-                      style: const TextStyle(
-                          fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                        height: 200,
+                        child: photo == null
+                            ? const CircleAvatar(
+                                backgroundImage:
+                                    AssetImage('Assets/images/avatar.jpg'),
+                                radius: 80,
+                              )
+                            :
+                            // CircleAvatar(
+                            //     backgroundImage: FileImage(
+                            //       File(photo!),
+                            //     ),
+                            //     radius: 90,
+                            //   ),
+                            SizedBox(
+                                child: Image(
+                                  image: FileImage(
+                                    File(photo!),
+                                  ),
+                                ),
+                              )),
+                  ],
+                ),
               ),
-            ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 50,
+                        child: Text(
+                          'NAME :  $name',
+                          style: const TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: Text(
+                          'CLASS :  $class_',
+                          style: const TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: Text(
+                          'AGE : $age',
+                          style: const TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: Text(
+                          'PHONE NO : $rollno',
+                          style: const TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
